ae61ce93598eccf31f0909694b867685d21d2f33
Dmitry Pankratov <dmitry@pankratov.net>
2021-10-16 12:21:32 +0200

    Added root_dse example

---
+++ examples/root_dse.rs
@@ -0,0 +1,17 @@
+use futures::TryStreamExt;
+
+use ldap_rs::{client::LdapClient, request::SearchRequestBuilder};
+
+#[tokio::main]
+async fn main() {
+    let mut client = LdapClient::builder("ldap.forumsys.com")
+        .build_and_connect()
+        .await
+        .unwrap();
+
+    let req = SearchRequestBuilder::new().filter("(objectClass=*)").build().unwrap();
+
+    let result = client.search(req).await.unwrap();
+    let items = result.try_collect::<Vec<_>>().await.unwrap();
+    println!("{:#?}", items);
+}
