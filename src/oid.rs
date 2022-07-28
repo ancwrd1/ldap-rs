0a8c0d43657b65ccb12d33b58f72c4f23c8d53c6
Dmitry Pankratov <dmitry@pankratov.net>
2022-07-28 17:45:21 +0200

    Aligned WHOAMI_OID type

--- src/oid.rs
+++ src/oid.rs
@@ -4,7 +4,7 @@
 pub const STARTTLS_OID: &[u8] = b"1.3.6.1.4.1.1466.20037";
 
 /// WHOAMI extended operation
-pub const WHOAMI_OID: &str = "1.3.6.1.4.1.4203.1.11.3";
+pub const WHOAMI_OID: &[u8] = b"1.3.6.1.4.1.4203.1.11.3";
 
 /// Notice of disconnection response sent by the server
 pub const NOTICE_OF_DISCONNECTION_OID: &[u8] = b"1.3.6.1.4.1.1466.20036";
