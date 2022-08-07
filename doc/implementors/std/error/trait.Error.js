a0f52059d11e6b84966fb9f131bb2cf73ee91bce
Dmitry Pankratov <dmitry@pankratov.net>
2022-08-07 08:18:46 +0200

    Updated documentation for 0.3.0

--- doc/implementors/std/error/trait.Error.js
+++ doc/implementors/std/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["ldap_rs"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.61.0/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"ldap_rs/error/enum.Error.html\" title=\"enum ldap_rs::error::Error\">Error</a>","synthetic":false,"types":["ldap_rs::error::Error"]}];
+implementors["ldap_rs"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.62.0/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"ldap_rs/error/enum.Error.html\" title=\"enum ldap_rs::error::Error\">Error</a>","synthetic":false,"types":["ldap_rs::error::Error"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
