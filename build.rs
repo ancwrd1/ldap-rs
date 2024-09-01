b26563ad83ce618d5e05acaba576e3118abfe274
Dmitry Pankratov <dmitry@pankratov.net>
2024-09-01 23:00:22 +0200

    Fided clippy warnings

--- build.rs
+++ build.rs
@@ -3,5 +3,8 @@ compile_error!("Please specify one of tls-native-tls or tls-rustls, but not both
 
 fn main() {
     #[cfg(any(feature = "tls-native-tls", feature = "tls-rustls"))]
-    println!("cargo:rustc-cfg=tls");
+    {
+        println!("cargo::rustc-check-cfg=cfg(tls)");
+        println!("cargo:rustc-cfg=tls");
+    }
 }
